7945524fcb4b0abda4c3afec5ac6864d93218f20
John Youn <john@Johns-MacBook-Pro.local>
2020-09-15 21:55:07 -0700

    finishing

---
+++ team-builder/src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Form from './components/Form';
+import Team from './components/Team';
+
+function App() {
+  // setTeam([...teams, team]);
+
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
